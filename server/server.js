f19a5fc7885dcd3d629200c1d8a2466a3aa8ea31
Toby Warburton <tobywarburton10@gmail.com>
2023-10-25 13:56:23 -0700

    server pdates

--- server/server.js
+++ server/server.js
@@ -2,9 +2,22 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const {ApolloServer} = require ('apollo-server-express')
+const typeDefs = require('./schema/typeDefs');
+const resolvers = require('./schema/resolvers');
+const db = require('./config/connection');
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({req}) => ({req}),
+});
+
+server.applyMiddleware({app});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,14 +6,12 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: context =>  {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    const { req } = context ;
 
     // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
+    const token = req.headers.athorization || '';
 
     if (!token) {
       return res.status(400).json({ message: 'You have no token!' });
